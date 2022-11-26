3902f4b32a1ba898969a1e1fd18c6bf4ebd351dc
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2022-11-26 20:11:44 -0300

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,17 @@
+var valido;
 
+function login() {
+    var senha;
+    var gmail;
+    var cpf;
+    var rg;
+    var codigo;
+
+    if (valido == 0) {
+        alert('senha ou email incorretos');
+    }
+
+    if (senha = "18007402009853199966805300" && gmail = "null" && cpf = "000.000.000-00" && rg = null && codigo = "000a000a000") {
+        valido = 1;
+    }
+}
