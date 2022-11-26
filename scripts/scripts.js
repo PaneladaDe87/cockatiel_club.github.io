489e5c4e2c20865951c68621fb94c9ff3e60a04e
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2022-11-26 20:25:37 -0300

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,17 @@
 var valido;
 
 function login() {
-    var senha;
-    var gmail;
-    var cpf;
-    var rg;
-    var codigo;
+    var senha = documents.login-inputs.senha.value;
+    var gmail = documents.login-inputs.gmail.value;
+    var cpf = documents.login-inputs.cpf.value;
+    var rg = documents.login-inputs.rg.value;
+    var codigo = documents.login-inputs.codigo.value;
+
+    gmail = gmail.toLowerCase();
+    senha = senha.toLowerCase();
+    cpf = cpf.toLowerCase();
+    rg = rg.toLowerCase();
+    codigo = codigo.toLowerCase();
 
     if (valido == 0) {
         alert('senha ou email incorretos');
@@ -13,5 +19,6 @@ function login() {
 
     if (senha = "18007402009853199966805300" && gmail = "null" && cpf = "000.000.000-00" && rg = null && codigo = "000a000a000") {
         valido = 1;
+        window.location="index.html";
     }
 }
